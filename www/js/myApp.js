443a011c94dca552d7895c64a470ae54bf2c3ba5
Edgar <edgarfhb@hotmail.com>
2019-05-07 21:47:19 +0100

    v1

--- www/js/myApp.js
+++ www/js/myApp.js
@@ -1,48 +1,12 @@
-// Initialize app
-var myApp = new Framework7();
+document.addEventListener('deviceready',this.onDeviceRedy, false);
 
-
-// If we need to use custom DOM library, let's save it to $$ variable:
-var $$ = Dom7;
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
-});
-
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+function onDeviceRedy(){
+    console.log("device ready");
+    //GET THE FILE READY
     tryingFile();
+    //CALL LOCATION WHEN DEVICE US READY
     getLocation();
-});
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
-
-////////////////////////////////////////////////////////////////
+}
 
 //GLOBAL VARIABLES 
 var date;
@@ -281,5 +245,8 @@ function limpia() {
         console.log('Could not delete Config file. '+error);
             });
     }
+
+
+
 
 
