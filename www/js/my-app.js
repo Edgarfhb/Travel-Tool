aff2622d1725909630669b9fd1431f1a52b69219
Edgar <edgarfhb@hotmail.com>
2019-05-07 14:27:15 +0100

    v1.o

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,12 +1,50 @@
-document.addEventListener('deviceready',this.onDeviceRedy, false);
+// Initialize app
+var myApp = new Framework7();
 
-function onDeviceRedy(){
-    console.log("device ready");
-    //GET THE FILE READY
-    tryingFile();
-    //CALL LOCATION WHEN DEVICE US READY
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
     getLocation();
-}
+    tryingFile();
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+})
+
+////////////////////////////////////////////////////////////////
 
 //GLOBAL VARIABLES 
 var date;
@@ -39,8 +77,9 @@ function geoCallback(position){
     initMap();  //MAP FROM LOCATION
 }
 
-function onError(msg){
-    console.log(msg);
+function onError(error){
+    alert('Error please check your GPS on Settings: '    + error.code    + '\n' +
+              'Please Active Your GPS' + error.message + '\n');
 }
 
 function initMap() {
@@ -244,4 +283,3 @@ function limpia() {
         console.log('Could not delete Config file. '+error);
             });
     }
-
